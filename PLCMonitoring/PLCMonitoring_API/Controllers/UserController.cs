1913b3367eceb81990b1cc64e02c3ce08bff5d87
Gabriel Montalvo Puente <gabriel-montalvo@nissin-mfg.com.mx>
2023-09-28 16:02:17 -0600

    first commit :)

---
+++ PLCMonitoring/PLCMonitoring_API/Controllers/UserController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PLCMonitoring_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly IHttpContextAccessor _contextAccessor;
+        public UserController(IHttpContextAccessor _httpContext)
+        {
+            _contextAccessor = _httpContext;
+        }
+
+        [HttpGet]
+        public string GetCurrentUserName ()
+        {
+            string? username = _contextAccessor.HttpContext.User.Identity.Name;
+            return username ?? "Not found";
+        }
+    }
+}
